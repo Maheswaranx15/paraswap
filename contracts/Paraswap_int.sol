feb471f158e192dd51bea60bfaf64ee920d023e1
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-02-19 18:27:01 +0530

    initial commit

---
+++ contracts/Paraswap_int.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "./IAugustusSwapper.sol";
+import "./IParaswap.sol";
+import "./lib/Utils.sol";
+
+contract ParaswapIntegration {
+    IAugustusSwapper augustusSwapperContract;
+    IParaswap paraswapContract;
+
+    constructor(address _augustusSwapperAddress, address _paraswapAddress) {
+        augustusSwapperContract = IAugustusSwapper(_augustusSwapperAddress);
+        paraswapContract = IParaswap(_paraswapAddress);
+    }
+
+    function swap(Utils.SellData calldata data) external {
+        uint256 receivedAmount = paraswapContract.multiSwap(data);
+    }
+}
