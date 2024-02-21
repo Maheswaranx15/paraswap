571700c2dd49e1ed221f2460912c3939d791b471
Maheswaranx15 <maheshwaran66648@gmail.com>
2024-02-21 21:28:28 +0530

    interfaces configured

--- contracts/Paraswap_int.sol
+++ contracts/Paraswap_int.sol
@@ -14,9 +14,33 @@ contract ParaswapIntegration {
         paraswapContract = IParaswap(_paraswapAddress);
     }
 
-    function swap(Utils.SellData calldata data) external {
-        uint256 receivedAmount = paraswapContract.multiSwap(data);
-        augustusSwapperContract.transferTokens(data.destToken, recipient, receivedAmount);
-
+   function swap(Utils.SimpleData calldata _data) external nonReentrant {
+        require(_data.beneficiary == msg.sender, "beneficiary != msg.sender");
+        require(
+            tokens[_data.fromToken][_data.beneficiary] >= _data.fromAmount,
+            "Balance insufficient");
+        require(_data.fromToken == BTCT_ADDR,"fromToken != BTCT_ADDR");
+        tokens[_data.fromToken][_data.beneficiary] = tokens[_data.fromToken][
+            _data.beneficiary
+        ].sub(_data.fromAmount);
+        
+        _doSimpleSwap(_data); //no received amount, tokens to go user's wallet
     }
+
+    /// @dev _doSimpleSwap - performs paraswap transaction - BALANCE & TOKEN CHECKS MUST OCCUR BEFORE CALLING THIS
+    /// @param _data data from API call that is ready to be sent to paraswap interface
+    function _doSimpleSwap(Utils.SimpleData calldata _data) internal {
+        //address proxy = IAugustusSwapper(paraswapAddress).getTokenTransferProxy();
+
+        IERC20(_data.fromToken).safeIncreaseAllowance(
+            IAugustusSwapper(paraswapAddress).getTokenTransferProxy(), 
+            _data.fromAmount
+        );
+
+        IParaswap(paraswapAddress).simpleSwap(_data);
+    }
+
+
 }
+
+
